d06fe26bc38fa2470fce5092bae114bf2b957007
Bruno Moreira dos Santos <bmds.dev@gmail.com>
2019-01-18 15:02:51 -0200

    Solution to exercise 04

--- exercise_04.py
+++ exercise_04.py
@@ -1,12 +1,12 @@
 # source: https://www.practicepython.org/exercise/2014/02/26/04-divisors.html
 
-def divisors(num):
-    return [x for x in range(2, num) if num%x==0]
+def divisors(num=None):
+    print('Input a number and I\'ll show you all their divisors (if there\'re any)!')
+    input_num = 0
+    try:
+        input_num = int(input(''))
+        return [x for x in range(2, input_num) if input_num%x==0]
+    except ValueError:
+        print('You should input a valid number!')
 
-print('Input a number and I\'ll show you all their divisors (if there\'re any)!')
-num = 0
-try:
-    num = int(input(''))
-    print(divisors(num))
-except ValueError:
-    print('You should input a valid number!')
+print(divisors())
---
+++ exercise_11.py
@@ -0,0 +1,2 @@
+# source: https://www.practicepython.org/exercise/2014/04/16/11-check-primality-functions.html
+from exercise_04 import divisors
