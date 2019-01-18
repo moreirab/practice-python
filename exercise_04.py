9d5f574163b29380c33bd2cb62ea72d174887e07
Bruno Moreira dos Santos <bmds.dev@gmail.com>
2019-01-18 15:36:51 -0200

    Solution to exercises 4 and 11

--- exercise_04.py
+++ exercise_04.py
@@ -1,12 +1,15 @@
 # source: https://www.practicepython.org/exercise/2014/02/26/04-divisors.html
 
-def divisors(num=None):
+def check_divisors(num):
+    return [x for x in range(2, num) if num%x==0]
+
+def divisors():
     print('Input a number and I\'ll show you all their divisors (if there\'re any)!')
     input_num = 0
     try:
-        input_num = int(input(''))
-        return [x for x in range(2, input_num) if input_num%x==0]
+        input_num = int(input())
+        if input_num == 0 or input_num == 1:
+            raise ValueError
+        print(divisors(num=input_num))
     except ValueError:
         print('You should input a valid number!')
-
-print(divisors())
--- exercise_11.py
+++ exercise_11.py
@@ -1,2 +1,17 @@
 # source: https://www.practicepython.org/exercise/2014/04/16/11-check-primality-functions.html
-from exercise_04 import divisors
+from exercise_04 import check_divisors
+input_num = 0
+
+print('Input a number and I\'ll show you whether this is a prime number or not!')
+try:
+    input_num = int(input())
+    if input_num == 0 or input_num == 1:
+        raise ValueError
+    is_prime = len(check_divisors(input_num)) == 0
+    if is_prime:
+        print('This number is prime!')
+    else:
+        print('This number is not prime!')
+
+except ValueError:
+    print('You should input a valid number!')
